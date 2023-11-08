c11aff5f7ff167262ab7dae8ada18e7c20fa0f2f
ebanlee1204 <ebanlee1204@gmail.com>
2023-11-08 15:57:22 +0900

    1,2주차 파일 생성

---
+++ 1주차/1주차/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1주차
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your height and weight: ");
+            string input = Console.ReadLine();
+
+            string[] height_weight = input.Split(' ');
+
+            float height = float.Parse(height_weight[0]) / 100;
+            float weight = float.Parse(height_weight[1]);  
+
+            Console.WriteLine("Your BMI is: {0}", weight / (height * height));
+        }
+    }
+}
